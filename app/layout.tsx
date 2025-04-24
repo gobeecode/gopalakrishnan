2152664d1e3316a98f51e4dd76e8e2f1e561a1ad
gobeecode <gopalakrishnan.work@gmail.com>
2025-04-24 08:17:39 +0530

    Pinterest verify meta tag added.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { PageTransition } from '@/components/PageTransition'
 import { Navbar } from '@/components/Navbar'
 import { SmoothScrollProvider } from '@/components/SmoothScrollProvider'
+import Head from 'next/head'
 
 export const metadata: Metadata = {
   title: 'Gopalakrishnan | Portfolio',
@@ -15,6 +16,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="p:domain_verify" content="7eb8a7284db489b206f0a7b0befd8d9d"/>
+      </head>
       <body className="bg-white text-black dark:bg-gray-900 dark:text-white transition-colors duration-300">
           <SmoothScrollProvider>
             <Navbar />
